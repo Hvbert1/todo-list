6fd46d4ea776b7998f3e4e14b7def5f27df9d3fe
Hvbert1 <henryjiafu@gmail.com>
2023-03-15 17:02:48 +1100

    add delete project function + project array

--- src/project.js
+++ src/project.js
@@ -1,5 +1,6 @@
 import task, { createTask , appendTask, displayTask, resetTask} from './task.js'
 
+export let projectList= [];
 export let selectedProject;
 
 const project = (name) => {
@@ -10,23 +11,48 @@ const project = (name) => {
 export function createProject() {
     let projectName = document.getElementById("project").value;
     const newProject = project(projectName);
+
     return newProject;
 }
 
+export function appendProject() {
+    let newProject = createProject();
+
+    projectList.push(newProject);
+    displayProject();
+}
+
 export function displayProject() {
-    let project = createProject();
-    let projectDiv = document.createElement("div");
-    let projectValue = document.getElementById("project").value;
-    projectDiv.innerHTML = projectValue;
+    resetProject();
 
-    document.getElementById("projectArea").appendChild(projectDiv);
+    for (let i = 0; i < projectList.length; i++) {
+        let projectDiv = document.createElement("div");
+        let projectDel = document.createElement("div");
 
-    //create and append new task divs onto the taskArea
-    projectDiv.addEventListener("click", function(){
-        resetTask();
-        displayTask(project);
-        selectedProject = project;
-    });
+        projectDiv.id = i;
+        projectDiv.innerHTML = projectList[i].name;
+
+        document.getElementById("projectSpace").appendChild(projectDiv);
+        projectDiv.appendChild(projectDel);
+
+        projectDiv.addEventListener("click", function(e){
+            selectedProject = projectList[e.target.id];
+            displayTask(projectList[e.target.id]);
+        });
+
+        projectDel.addEventListener("click", function(){
+            delProject(projectDiv);
+        });
+    }
+}
+
+export function delProject(project) {
+    projectList.splice(project.id, 1)
+    displayProject();
+}
+
+export function resetProject() {
+    document.getElementById("projectSpace").innerHTML = "";
 }
 
 export default project;
