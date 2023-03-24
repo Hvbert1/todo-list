eb3bff9264df617bb8a2e1abdb0143999db11ded
Hvbert1 <henryjiafu@gmail.com>
2023-03-24 12:17:49 +1100

    save input changes to task

--- src/task.js
+++ src/task.js
@@ -27,12 +27,12 @@ export function displayTask(project) {
 
     for (let i = 0; i < project.tasks.length; i++) {
         let taskDiv = document.createElement("div");
+        let nameContainer = document.createElement("div");
         let taskName = document.createElement("input");
         let taskDesc = document.createElement("input");
         let taskDate = document.createElement("input");
         let taskPrio = document.createElement("div");
-        let taskCheck = document.createElement("div");
-        let hiddenTask = document.createElement("div");
+        let taskCheck = document.createElement("i");
         
         taskName.setAttribute("type", "text");
         taskDesc.setAttribute("type", "text");
@@ -42,38 +42,59 @@ export function displayTask(project) {
         taskDesc.value  = project.tasks[i].description;
         taskDate.value = project.tasks[i].dueDate;
         taskPrio.innerHTML = project.tasks[i].priority;
+        taskCheck.textContent = "check_circle";
 
         taskName.classList.add("taskName");
         taskDesc.classList.add("taskDesc");
         taskDate.classList.add("taskDate");
         taskPrio.classList.add("taskPrio");
-        taskCheck.classList.add("taskCheck");
+        taskCheck.classList.add("material-icons");
+        nameContainer.classList.add("nameContainer");
 
         taskDiv.id = i;
-        taskDiv.classList.add('expendable');
 
-        hiddenForm.id = "hiddenForm"
-        hiddenForm.classList.add("hidden");
-
-        taskDiv.append(taskName);
-        taskDiv.append(taskDesc);
-        taskDiv.append(taskDate);
-        taskDiv.append(taskPrio);
         taskDiv.append(taskCheck);
+        taskDiv.append(nameContainer);
+        taskDiv.append(taskDesc);
+        taskDiv.append(taskPrio);
+        nameContainer.append(taskDate);
+        nameContainer.append(taskName);
 
         taskCheck.addEventListener('click', function() {
             checkTask(taskDiv);
         })
 
+        taskName.addEventListener('change', function(e) {
+            const selectedDiv = e.target;
+            const parentElement = selectedDiv.parentNode;
+            const grandParentElement = parentElement.parentNode;
+            const taskId = grandParentElement.id;
+
+            selectedProject.tasks[taskId].title = selectedDiv.value;
+            saveProjects(); 
+        });
+
+        taskDesc.addEventListener('change', function(e) {
+            const selectedDiv = e.target;
+            const parentElement = selectedDiv.parentNode;
+            const taskId = parentElement.id;
+   
+            selectedProject.tasks[taskId].description = selectedDiv.value;
+            saveProjects(); 
+        });
+
+        taskDate.addEventListener('change', function(e) {
+            const selectedDiv = e.target;
+            const parentElement = selectedDiv.parentNode;
+            const grandParentElement = parentElement.parentNode;
+            const taskId = grandParentElement.id;
+
+            selectedProject.tasks[taskId].dueDate = selectedDiv.value;
+            saveProjects(); 
+        });
+
         document.getElementById("taskSpace").appendChild(taskDiv);
     }
-}
-
-export function checkTask(task) {
-    selectedProject.tasks.splice(task.id, 1);
-    displayTask(selectedProject);
-
-    saveProjects();
 }
 
 export function resetTask() {
